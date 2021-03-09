65175126eeb832faa5cd71a1aa08afe2a3b3f02a
Jessica Chester <chesterjessica10@gmail.com>
2021-03-09 11:44:06 -0600

    third data analysis

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -7,3 +7,9 @@ SELECT employees.first_name, employees.last_name, employees.hire_date
 FROM employees
 WHERE employees.hire_date LIKE '%1986';
 
+SELECT dept_manager.emp_no, employees.last_name, employees.first_name, departments.dept_no, departments.dept_name
+FROM dept_manager
+LEFT JOIN departments ON
+departments.dept_no = dept_manager.dept_no
+Left JOIN employees ON
+employees.emp_no = dept_manager.emp_no;
