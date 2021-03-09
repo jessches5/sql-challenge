07a6206437db60ec03cfba62e65cede2ebb6c0d8
Jessica Chester <chesterjessica10@gmail.com>
2021-03-09 11:00:32 -0600

    second data analysis

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -3,3 +3,7 @@ FROM employees
 INNER JOIN salaries ON
 salaries.emp_no = employees.emp_no;
 
+SELECT employees.first_name, employees.last_name, employees.hire_date
+FROM employees
+WHERE employees.hire_date LIKE '%1986';
+
